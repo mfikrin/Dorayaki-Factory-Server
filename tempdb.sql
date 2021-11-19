4e6e700592f7939424b66d22bff11b30c539caf9
myahyaibrahim <13519091@std.stei.itb.ac.id>
2021-11-19 11:19:07 +0700

    add query to db

--- tempdb.sql
+++ tempdb.sql
@@ -36,6 +36,35 @@ CREATE TABLE users(
     password VARCHAR(100) NOT NULL
 );
 
+DROP TABLE IF EXISTS request;
+CREATE TABLE request(
+    request_id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    dora_id INTEGER NOT NULL,
+    req_qty INTEGER NOT NULL,
+    status VARCHAR(100) NOT NULL,
+    CONSTRAINT fkreqdora FOREIGN KEY(dora_id) REFERENCES dora(dora_id)
+);
+
+DROP TABLE IF EXISTS request_log;
+CREATE TABLE request_log(
+    log_id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    request_id INTEGER NOT NULL,
+    ip VARCHAR(100) NOT NULL,
+    timestamp_req TIMESTAMP NOT NULL, 
+    CONSTRAINT fklog FOREIGN KEY(request_id) REFERENCES request(request_id)
+);
+
 INSERT INTO users (username,email,password) VALUES ('admin','shokomakinohara10@gmail.com','ayaya');
+
+INSERT INTO bahan (bahan_name, bahan_qty) VALUES ('Tepung',100);
+
+INSERT INTO dora (dora_name) VALUES ('Jeruk');
+
+INSERT INTO resep (bahan_id, dora_id, resep_qty) VALUES (1,1,30);
+
+INSERT INTO request (dora_id, req_qty, status) VALUES (1,5,'Pending');
+
+INSERT INTO request_log (request_id, ip, timestamp_req) VALUES (1,'123.123.123','2021-11-19 06:45:10');
+
 
 -- will add request + log later on
